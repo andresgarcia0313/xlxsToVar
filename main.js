b933e61cee639c726e0e8b964f2ade498e04d272
Andrés García <andresgarcia0313@gmail.com>
2024-02-13 13:51:58 -0500

    Segunda versión conversión en node y en navegador

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-const XLSX = require('xlsx');
-const fs = require('fs');
-const nombreArchivoExcel = 'archivo.xlsx';
-const workbook = XLSX.readFile(nombreArchivoExcel);
-const sheetName = workbook.SheetNames[0];
-const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
-const jsonData = JSON.stringify(data);
-console.log(jsonData);
-//Imprimir de forma tabulada en
-fs.writeFileSync('datos.js', `const datos = ${jsonData};`);
+//Versión compatible con Node.js en el index.html esta la versión compatible con el navegador
 
-console.log('Datos convertidos y guardados en datos.js');
+const XLSX = require("xlsx");
+const path = require("path");
+/**
+ * Function to convert xlsx file to object
+ *
+ * @param {*} fileName
+ * @returns Array [{name: string, data: Array}]
+ */
+const xlsxToObject = (fileName) => {
+  const xlsxAPI = XLSX.readFile(fileName);
+  const spreadSheets = [];
+  for (let name of xlsxAPI.SheetNames)
+    spreadSheets.push({
+      name: name,
+      data: XLSX.utils.sheet_to_json(xlsxAPI.Sheets[name]),
+    });
+  return spreadSheets;
+};
+
+const filePath = path.join(__dirname, "1file.xlsx");
+const fileData = xlsxToObject(filePath);
+for (let sheet of fileData) 
+  console.dir(sheet);
+
+console.log("Fin del programa");
